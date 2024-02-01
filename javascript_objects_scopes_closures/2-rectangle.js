2fce46c2f23105731af5eac8c4341208a538b2e2
yhijazi24 <yahyahijazi643@gmail.com>
2024-02-02 00:04:20 +0100

    m

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -5,6 +5,7 @@ class Rectangle {
         this.width = w;
         this.height = h;
       } else {
+        this.Rectangle = {}
         this.width = undefined;
         this.height = undefined;
       }
