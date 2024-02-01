50ea9ba20c1a333cb46a15705b1c9e925a227830
yhijazi24 <yahyahijazi643@gmail.com>
2024-02-01 23:34:34 +0100

    m

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+const firstLine = "C is fun"
+const secondLine = "Python is cool"
+const thirdLine = "JavaScript is amazing"
+
+console.log(firstLine)
+console.log(secondLine)
+console.log(thirdLine)
+
+
