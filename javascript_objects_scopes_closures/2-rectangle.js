68aa4cbcc0a0d407b2daf3bc50e5e77296cee0ba
yhijazi24 <yahyahijazi643@gmail.com>
2024-02-02 00:02:26 +0100

    n

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-        if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
-            
-            this.width = w;
-            this.height = h;
-          } else {
-          
-            this.width = undefined;
-            this.height = undefined;
-          }
+      if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
+        this.width = w;
+        this.height = h;
+      } else {
+        this.width = undefined;
+        this.height = undefined;
+      }
     }
-}
-module.exports = Rectangle;
+  }
+
+  module.exports = Rectangle;
