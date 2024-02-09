ebced96b9662c635f7afe96530d275b1e68eb2e4
yhijazi24 <yahyahijazi643@gmail.com>
2024-02-09 21:32:58 +0100

    j

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-        this.width = w > 0 && Number.isInteger(w) ? w : undefined;
-        this.height = h > 0 && Number.isInteger(h) ? h : undefined;
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
