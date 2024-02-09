a627ad8503af602e9564359da485b18eb7d5bdb4
yhijazi24 <yahyahijazi643@gmail.com>
2024-02-09 21:36:20 +0100

    j

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+        
+        return {};
+      } else {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print() {
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          let line = '';
+          for (let j = 0; j < this.width; j++) {
+            line += 'X';
+          }
+          console.log(line);
+        }
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
+  
