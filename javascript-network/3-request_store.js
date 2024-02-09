44e9078c8a4b13263590a72893ff34ce3b8fa869
yhijazi24 <yahyahijazi643@gmail.com>
2024-02-09 21:47:59 +0100

    k

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    
+    if (response.statusCode !== 200) {
+        console.error(`Error: ${response.statusCode}`);
+        return;
+    }
+
+    fs.writeFile(filePath, body, 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`Contents of ${url} saved to ${filePath}`);
+    });
+});
